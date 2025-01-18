6267d70e98c50704fb51c895c2c07a3a79d1cff8
Krishnayanb <139518423+Krishnayanb@users.noreply.github.com>
2025-01-19 03:27:22 +0530

    Update and rename index.html to index.php
    Signed-off-by: Krishnayanb <139518423+Krishnayanb@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,109 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Doctor Plus a Medical Category Flat bootstrap Responsive website Template | Home :: w3layouts</title>
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Doctor Plus Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //Custom Theme files -->
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script> 
+<!-- //js -->	
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<!--header-->
+	<?php include("inc/header.php"); ?>
+	<!--//header-bottom-->
+	
+	<div class="container-fluid p-3em mb-5 banner">
+        <div class="row g-0 align-items-center flex-column-reverse flex-lg-row">
+        </div>
+    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img class="banner-text-h1" src="images/banner-text-h1.png" height="100px">
+				
+				<div class="col-md-6">
+					<div class="banner-text">
+						<h3>Empowering Your Journey to Motherhood</h3>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="banner-text">
+						<h3>মায়ের আশ্বাস, প্রতিটি পদক্ষেপে</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>  
+	<!--//banner-bottom-->
+	<!--gallery-->
+	<div class="gallery" id="gallery">
+		<div class="col-md-6 gallery-left">
+			
+		</div>
+		<div class="col-md-6 gallery-right">
+			<div class="gallery-grid-a">
+				
+			</div>	
+			<div class="gallery-grid-b">		
+				
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+	<!--//gallery-->
+	<!--work-->
+	<div class="work">		
+		<div class="container">	
+			<div class="work-title">
+				<h3>More Facilities Coming Soon</h3>
+				<p>This is an ongoing project and we hope to add more functionalities in this website in near future</p>
+			</div>
+			<div class="col-md-4 work-grids">
+				<ul>
+					<li><img src="images/icon1.png" alt=""></li>
+					<li>
+						<h4>Video-calling with doctor</h4>
+						<p>A face-time option shall be available which will implement a private virtual room with the doctor and the patient.</p>
+					</li>
+				</ul>
+			</div>	
+			<div class="col-md-4 work-grids">
+				<ul>
+					<li><img src="images/chat.png" alt=""></li>
+					<li>
+						<h4>Chat with Doctor</h4>
+						<p>An integrated private chat room with the doctor to have a hassle-free chatting experience. Talk to the doctor like you talk to any friend without the fear of getting judged.</p>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-4 work-grids">
+				<ul>
+					<li><img src="images/period.png" alt=""></li>
+					<li>
+						<h4>Period tracker</h4>
+						<p>A period tracking system shall be available to keep track of your menstrual cycle.</p>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!--//work-->
+	
+	<!--footer-->
+	<?php include("inc/footer.php");?>
+	<!--//footer-->
+	
+    <script src="js/bootstrap.js"> </script>
+</body>
+</html>
