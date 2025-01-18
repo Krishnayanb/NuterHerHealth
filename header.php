d242c6e7a31abac4d97666a80e0c3774fc631c1a
Krishnayanb <139518423+Krishnayanb@users.noreply.github.com>
2025-01-19 04:12:06 +0530

    Update header.php
    Signed-off-by: Krishnayanb <139518423+Krishnayanb@users.noreply.github.com>

--- header.php
+++ header.php
@@ -1,43 +1,47 @@
 <div class="header">
-	<div class="container-fluid p-1.7em mb-5">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="header-logo">
-					<a href="index.php"><img src="LOGO1.jpg" alt="logo" width="220px "height="220px" /></a>					
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="header-banner">
-					<a href="index.php"><img src="nurture.png" alt="logo" width="420px" height="450px"/></a>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="header-info">
-					<h1><a href="login.php"><span data-hover="Login" class="login">Login</span></a>
-					</h1>
-				</div>	
-			</div>	
-			<div class="clearfix"> </div>
-		</div>
-	</div>	
-</div>
-<div class="header-bottom">
-		<div class="container">
-			<!--top-nav-->
-			<div class="top-nav cl-effect-5">
-				<span class="menu-icon"><img src="menu-icon.png" alt=""/></span>		
-				<ul class="nav1">
-					<li><a href="index.php"><span data-hover="Home">Home</span></a></li>
-					<li><a href="about.php" > <span data-hover="About">About</span></a></li>
-					<li><a href="appointment.php"> <span data-hover="Appointment">Appointment</span></a></li>
-					<li><a href="awareness.php"> <span data-hover="Awareness">Awareness</span></a></li>
-					<li><a href="contact.php"> <span data-hover="Contact">Contact</span></a></li>
-				</ul>
-			</div>
-			<!--//top-nav-->
-			
-			<div class="clearfix"> </div>
-		</div>
+    <div class="container-fluid p-1.7em mb-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="header-logo">
+                    <a href="index.php">
+                        <img src="LOGO1.jpg" alt="Company Logo" class="img-fluid" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="header-banner">
+                    <a href="index.php">
+                        <img src="nurture.png" alt="Nurture Logo" class="img-fluid" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="header-info text-center">
+                    <h1>
+                        <a href="login.php">
+                            <span data-hover="Login" class="login">Login</span>
+                        </a>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-    
+<div class="header-bottom">
+    <div class="container">
+        <!-- Top Navigation -->
+        <nav class="top-nav cl-effect-5">
+            <span class="menu-icon">
+                <img src="menu-icon.png" alt="Menu Icon" />
+            </span>
+            <ul class="nav1">
+                <li><a href="index.php"><span data-hover="Home">Home</span></a></li>
+                <li><a href="about.php"><span data-hover="About">About</span></a></li>
+                <li><a href="appointment.php"><span data-hover="Appointment">Appointment</span></a></li>
+                <li><a href="awareness.php"><span data-hover="Awareness">Awareness</span></a></li>
+                <li><a href="contact.php"><span data-hover="Contact">Contact</span></a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
